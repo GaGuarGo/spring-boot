252345088b608a0188635ca27a0c86818c683479
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2023-10-31 09:16:03 -0300

    Conectando banco de Dados H2 com Spring Data

--- src/main/java/br/com/fatec/hellospringboot/entities/Client.java
+++ src/main/java/br/com/fatec/hellospringboot/entities/Client.java
@@ -1,33 +1,43 @@
 package br.com.fatec.hellospringboot.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;;
+
+// Usa-se um table quando o nome da tabela no BD tem um nome diferente da classe
+@Entity
+@Table(name = "TBL_CLIENT")
 public class Client {
-    
+
+    @Id
     private int id;
+
     private String name;
     private double balance;
 
-
-    //Gerado pelo Source Action -> Getters and Setters -> Selected All -> OK
-
+    // Gerado pelo Source Action -> Getters and Setters -> Selected All -> OK
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getBalance() {
         return balance;
     }
+
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
-    
 
 }
---
+++ src/main/java/br/com/fatec/hellospringboot/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package br.com.fatec.hellospringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fatec.hellospringboot.entities.Client;
+
+
+//Construindo um repositorio do springdata
+public interface ClientRepository extends JpaRepository<Client, Integer>{
+    
+
+
+}
