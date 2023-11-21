91d60eeab33ef4aef84227d6c6ced8bbdb58c79d
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2023-11-21 10:24:05 -0300

    Exceção de Entity not Found

---
+++ src/main/java/br/com/fatec/hellospringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.fatec.hellospringboot.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+//Pode Capturar Excecções
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    // Defini o tipo de Exceção
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandartError> entityNotFoundException(EntityNotFoundException e,
+            HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandartError error = new StandartError();
+
+        error.setStatus(status.value());
+        error.setError("Resource not found");
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(Instant.now());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(error);
+
+    }
+
+}
